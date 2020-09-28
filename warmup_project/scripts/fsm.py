eab3b69c2723fc72f3b4fd8ca2bcb1a1b82af1de
Abby Fry <abbymfry@gmail.com>
2020-09-27 23:32:49 -0400

    add fsm status

--- warmup_project/scripts/fsm.py
+++ warmup_project/scripts/fsm.py
@@ -19,7 +19,7 @@ class FiniteState():
         self.targ_heading = 0
         self.lidar_range_index = [0]
         self.lidar_range_values = [0]
-        self.lidar_scan = None
+        self.lidar_scan = [0]
         self.ang_vel = .55
         self.line_vel = .2
         self.state = "square" # states are "square" and "person"
@@ -27,15 +27,21 @@ class FiniteState():
 
     def process_scan(self, msg):
         self.lidar_scan = msg.ranges
+        self.lidar_range_index = []
+        self.lidar_range_values = []
         for i in range(0, 361):
             if np.isinf(self.lidar_scan[i]) == False:
                 self.lidar_range_values.append(self.lidar_scan[i])
                 self.lidar_range_index.append(i)
-        list_index = np.argmin(self.lidar_range_values)
-        self.targ_heading = self.lidar_range_index[list_index]
+        if len(self.lidar_range_index) == 0:
+            self.angular_controller()
+        else:
+            list_index = np.argmin(self.lidar_range_values)
+            self.targ_heading = self.lidar_range_index[list_index]
         self.check_person()
-        #print("targ_heading: "+str(self.targ_heading))
-        print("person?: " + str(self.person_present))
+        #print("person? " + str(self.person_present))
+        #print(self.targ_heading)
+
 
     def check_person(self):
         for i in self.lidar_scan:
@@ -45,23 +51,8 @@ class FiniteState():
             else:
                 self.person_present = False
 
-    def drive_square(self):
-        time.sleep(1)
-        r = rospy.Rate(10)
-        self.pub.publish(Twist(linear=Vector3(x=0, y=0), angular=Vector3(z=0)))
-        self.pub.publish(Twist(linear=Vector3(x=self.line_vel, y=0)))
-        time.sleep(5)
-        self.pub.publish(Twist(linear=Vector3(x=0, y=0), angular=Vector3(z=self.ang_vel)))
-        time.sleep(3)
-        self.pub.publish(Twist(angular=Vector3(z=0)))
-
-    def follow_person(self):
-        r = rospy.Rate(10)
-        self.move_to_person()
-        print("HI PERSON")
-        self.pub.publish(Twist(linear=Vector3(x=0, y=0), angular=Vector3(z=0)))
-
     def move_to_person(self):
+        self.pub.publish(Twist(linear=Vector3(x=0, y=0), angular=Vector3(z=0)))
         if 3 < self.targ_heading < 357:
             self.angular_controller()
         else:
@@ -69,13 +60,16 @@ class FiniteState():
             self.distance_follow()
     
     def angular_controller(self):
-        ang_vel = 0.008 * self.targ_heading
+        ang_vel = 0.03 * self.targ_heading
         self.pub.publish(Twist(angular=Vector3(z=ang_vel)))
-    
+
     def linear_controller(self):
-        lin_vel = 0.2 * self.lidar_scan[0]
+        if np.isinf(self.lidar_scan[0]):
+            lin_vel = 0
+        else:
+            lin_vel = 0.2 * self.lidar_scan[0]
         self.pub.publish(Twist(linear=Vector3(x=lin_vel)))
-    
+
     def distance_follow(self):
         print("distance: " + str(self.lidar_scan[0]))
         if self.lidar_scan[0] > self.following_distance:
@@ -83,13 +77,26 @@ class FiniteState():
         else:
             self.pub.publish(Twist(linear=Vector3(x=0)))
 
+    def drive_square(self):
+        self.line_vel = .2
+        self.ang_vel = .55
+        time.sleep(1)
+        for i in range(0, 4):
+            self.pub.publish(Twist(linear=Vector3(x=0, y=0), angular=Vector3(z=0)))
+            self.pub.publish(Twist(linear=Vector3(x=self.line_vel, y=0)))
+            time.sleep(5)
+            self.pub.publish(Twist(linear=Vector3(x=0, y=0), angular=Vector3(z=self.ang_vel)))
+            time.sleep(3)
+            self.pub.publish(Twist(angular=Vector3(z=0)))
 
     def run(self):
         r = rospy.Rate(10)
         while not rospy.is_shutdown():
             if self.person_present:
-                self.follow_person()
-            else:
+                pass
+                #self.move_to_person()
+            if not self.person_present:
+                print("====== DRIVING SQUAREEEEEEEEEE ========")
                 self.drive_square()
             r.sleep()
 
